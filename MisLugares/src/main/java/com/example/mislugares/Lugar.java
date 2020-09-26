b6df3cb06546570786a7a986ffec7eb2cb9b3c9a
fdurante <FDURANTE@ALU.UCAM.EDU>
2020-09-26 19:00:37 +0200

    Primer commit

---
+++ MisLugares/src/main/java/com/example/mislugares/Lugar.java
@@ -0,0 +1,126 @@
+package com.example.mislugares;
+
+import javax.xml.crypto.Data;
+
+public class Lugar {
+
+    private String nombre;
+    private String direccion;
+    private GeoPunto posicion;
+    private String foto;
+    private int telefono;
+    private String url;
+    private String comentario;
+    private long fecha;
+    private  float valoracion;
+
+    /*
+    GETTERS
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public GeoPunto getPosicion() {
+        return posicion;
+    }
+
+    /*
+    SETTERS
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setPosicion(GeoPunto posicion) {
+        this.posicion = posicion;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public void setFecha(long fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setValoracion(float valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public long getFecha() {
+        return fecha;
+    }
+
+    public float getValoracion() {
+        return valoracion;
+    }
+
+    /*
+    CONSTRUCTOR
+     */
+    public Lugar(String nombre, String direccion, double longitud,
+                 double latitud, int telefono, String url, String comentario,
+                 float valoracion) {
+        fecha = System.currentTimeMillis();
+        posicion = new GeoPunto(longitud, latitud);
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.url = url;
+        this.comentario = comentario;
+        this.valoracion = valoracion;
+    }
+
+    @Override
+    public String toString() {
+        return "Lugar{" +
+                "nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", posicion=" + posicion +
+                ", foto='" + foto + '\'' +
+                ", telefono=" + telefono +
+                ", url='" + url + '\'' +
+                ", comentario='" + comentario + '\'' +
+                ", fecha=" + fecha +
+                ", valoracion=" + valoracion +
+                '}';
+    }
+
+}
