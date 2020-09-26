1b3f06c4a663d4352eb87b3ec64904f6cec7b028
fdurante <FDURANTE@ALU.UCAM.EDU>
2020-09-26 19:05:36 +0200

    Primer commit

--- MisLugares/src/main/java/com/example/mislugares/GeoPunto.java
+++ MisLugares/src/main/java/com/example/mislugares/GeoPunto.java
@@ -61,7 +61,7 @@ public class GeoPunto {
         double dLon = Math.toRadians(longitud - punto.longitud);
         double lat1 = Math.toRadians(punto.latitud);
         double lat2 = Math.toRadians(latitud);
-        double a = Math.sin(dLat/2) * Math.sin((dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
         return c * RADIO_TIERRA;
     }
